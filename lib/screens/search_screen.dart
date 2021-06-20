df4bb06c1244763a84afee4970ba1b7d4d8c9024
shubham-rawal <shubhamrawal2001@gmail.com>
2021-06-21 01:29:51 +0530

    database updated, now stores contacts for personal users

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,30 +1,100 @@
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
 import 'chat_screen.dart';
 
-class SearchScreen extends StatelessWidget {
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  static const String id = 'SearchScreen';
-  String searchedUser;
-  String searchedEmail = 'rahul@gmail.com';
-  String searchedName = 'Rahul';
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
+FirebaseAuth _auth = FirebaseAuth.instance;
+final User user = _auth.currentUser;
+String searchedUser;
+String searchedEmail;
+String searchedName;
+Widget finalWidget;
+bool isSearchResultEmpty = true;
 
-  Future<Widget> searchForUser() async {
-    final userList = await _firestore.collection('users').get();
-    for (var user in userList.docs) {
-      searchedEmail = user.get('email');
-      if (searchedEmail == searchedUser) {
-        searchedName = user.get('name');
-        print(searchedName);
-      }
+Future<void> searchForUser() async {
+  final userList = await _firestore.collection('users').get();
+  for (var user in userList.docs) {
+    searchedEmail = user.get('email');
+    if (searchedEmail == searchedUser) {
+      //searchedUser = null;
+      searchedName = user.get('name');
+      isSearchResultEmpty = false;
+
+      print(searchedName);
+      break;
     }
+    if (searchedName == null) isSearchResultEmpty = true;
   }
+}
+
+class SearchScreen extends StatefulWidget {
+  static const String id = 'SearchScreen';
 
   @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
   Widget build(BuildContext context) {
+    final _searchController = TextEditingController();
+
+    void createFinalWidget() {
+      if (isSearchResultEmpty == false) {
+        finalWidget = Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              onTap: () async {
+                Navigator.pushNamed(context, ChatScreen.id);
+                _firestore
+                    .collection('users')
+                    .doc(user.uid)
+                    .collection('contacts')
+                    .add({
+                  'contactEmail': searchedEmail,
+                  'contactName': searchedName,
+                });
+              },
+              minVerticalPadding: 5.0,
+              contentPadding: EdgeInsets.all(10.0),
+              leading: CircleAvatar(
+                radius: 25.0,
+                backgroundColor: Colors.blueGrey.shade100,
+                child: Text(
+                  searchedName[0],
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              title: Text(
+                searchedName,
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              subtitle: Text(searchedEmail),
+            ),
+          ],
+        );
+      } else {
+        finalWidget = Center(
+          child: Text(
+            'Oops! No results found for this search. Please try again',
+            textAlign: TextAlign.center,
+          ),
+        );
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
@@ -50,8 +120,10 @@ class SearchScreen extends StatelessWidget {
                 Expanded(
                   flex: 6,
                   child: TextField(
+                    controller: _searchController,
                     onChanged: (value) {
                       searchedUser = value;
+                      print(searchedUser);
                     },
                     decoration: kTextFieldDecoration.copyWith(
                       hintText: 'Search for a user by email...',
@@ -61,7 +133,10 @@ class SearchScreen extends StatelessWidget {
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
-                      searchForUser();
+                      setState(() {
+                        searchForUser();
+                        createFinalWidget();
+                      });
                     },
                     child: CircleAvatar(
                       radius: 22.0,
@@ -77,38 +152,8 @@ class SearchScreen extends StatelessWidget {
             ),
             Text('Search Results : '),
             Expanded(
-              child: ListView.builder(
-                itemCount: 1,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    },
-                    minVerticalPadding: 5.0,
-                    contentPadding: EdgeInsets.all(10.0),
-                    leading: CircleAvatar(
-                      radius: 25.0,
-                      backgroundColor: Colors.blueGrey.shade100,
-                      child: Text(
-                        searchedName[0],
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      searchedName,
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
-                    ),
-                  );
-                },
+              child: Container(
+                child: finalWidget,
               ),
             ),
           ],
